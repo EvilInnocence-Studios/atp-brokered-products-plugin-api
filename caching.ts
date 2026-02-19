30f2fa43eca710781bc61f80ad8ad8f5fc1d674e
Andrew Wittrock <andy@wittrock.us>
2026-02-19 12:47:09 -0600

    Remove cache types

--- caching.ts
+++ caching.ts
@@ -1,5 +1,3 @@
-import { IBehavior } from "../core/cloudfront";
-
-export const brokerageCaching:IBehavior[] = [
+export const brokerageCaching = [
     {precedence: 1, pathPattern: "/brokerage*", cache: true },
 ];
